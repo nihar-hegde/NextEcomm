bd605afd3d79c3be9a1171b69c2be253a495be07
Nihar Hegde <niharhegde163@gmail.com>
2023-04-24 21:10:32 +0530

    doing new product form

--- pages/products.js
+++ pages/products.js
@@ -1,5 +1,15 @@
 import Layout from "@/components/Layout";
+import Link from "next/link";
 
 export default function Products() {
-  return <Layout>Products page here</Layout>;
+  return (
+    <Layout>
+      <Link
+        className="bg-blue-900 text-white rounded-md py-1 px-2"
+        href={"/products/new"}
+      >
+        Add new Product
+      </Link>
+    </Layout>
+  );
 }
