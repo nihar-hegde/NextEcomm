ba3de3350ef7e835e31b02b3c9f5720cfab50d5d
Nihar Hegde <niharhegde163@gmail.com>
2023-04-26 18:50:51 +0530

    adding add and edit product form connecting to db

---
+++ components/ProductForm.js
@@ -0,0 +1,54 @@
+import Layout from "@/components/Layout";
+import axios from "axios";
+import { redirect } from "next/dist/server/api-utils";
+import { useRouter } from "next/router";
+import { useState } from "react";
+
+export default function ProductForm({
+  title: existingTitle,
+  description: existingDescription,
+  price: existingPrice,
+}) {
+  const [title, setTitle] = useState(existingTitle || "");
+  const [description, setDescription] = useState(existingDescription || "");
+  const [price, setPrice] = useState(existingPrice || "");
+  const [goToProducts, setGoToProducts] = useState(false);
+  const router = useRouter();
+  async function createProduct(ev) {
+    ev.preventDefault();
+    const data = { title, description, price };
+    await axios.post("/api/products", data);
+    setGoToProducts(true);
+  }
+  if (goToProducts) {
+    router.push("/products");
+  }
+  return (
+    <form onSubmit={createProduct}>
+      <h1>New Product</h1>
+      <label>Product Name</label>
+      <input
+        type="text"
+        placeholder="Product Name"
+        value={title}
+        onChange={(ev) => setTitle(ev.target.value)}
+      />
+      <label>Discription</label>
+      <textarea
+        placeholder="Discription"
+        value={description}
+        onChange={(ev) => setDescription(ev.target.value)}
+      />
+      <label>Price (in USD)</label>
+      <input
+        type="number"
+        placeholder="Price"
+        value={price}
+        onChange={(ev) => setPrice(ev.target.value)}
+      />
+      <button type="submit" className="btn-primary">
+        Save
+      </button>
+    </form>
+  );
+}
--- pages/products/new.js
+++ pages/products/new.js
@@ -1,16 +1,11 @@
 import Layout from "@/components/Layout";
+import ProductForm from "@/components/ProductForm";
 
 export default function NewProduct() {
   return (
     <Layout>
       <h1>New Product</h1>
-      <label>Product Name</label>
-      <input type="text" placeholder="Product Name"></input>
-      <label>Discription</label>
-      <textarea placeholder="Discription"></textarea>
-      <label>Price (in USD)</label>
-      <input type="number" placeholder="Price" />
-      <button className="btn-primary">Save</button>
+      <ProductForm></ProductForm>
     </Layout>
   );
 }
