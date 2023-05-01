c9c211be24ac5bcd0556a9f67cbdb80646fe0e08
Nihar Hegde <niharhegde163@gmail.com>
2023-05-01 20:40:43 +0530

    uploading to aws is a success

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -1,5 +1,11 @@
 import multiparty from "multiparty";
+import mime from "mime-types";
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import fs from "fs";
+const bucketName = "nihar-next-ecommerce";
+
 import { resolve } from "styled-jsx/css";
+import { networkInterfaces } from "os";
 export default async function handle(req, res) {
   const form = new multiparty.Form();
   const { fields, files } = await new Promise((resolve, reject) => {
@@ -9,8 +15,32 @@ export default async function handle(req, res) {
     });
   });
   console.log("length: ", files.file.length);
+  const client = new S3Client({
+    region: "ap-south-1",
+    credentials: {
+      accessKeyId: process.env.S3_ACCESS_KEY,
+      secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+    },
+  });
+  const links = [];
+  for (const file of files.file) {
+    const ext = file.originalFilename.split(".").pop();
+    const newFilename = Date.now() + "." + ext;
+    console.log({ ext, file });
+    await client.send(
+      new PutObjectCommand({
+        Bucket: bucketName,
+        Key: newFilename,
+        Body: fs.readFileSync(file.path),
+        ACL: "public-read",
+        ContentType: mime.lookup(file.path),
+      })
+    );
+    const link = `https://${bucketName}.s3.amazonaws.com/${newFilename}`;
+    links.push(link);
+  }
 
-  res.json("ok");
+  res.json(links);
 }
 
 export const config = {
