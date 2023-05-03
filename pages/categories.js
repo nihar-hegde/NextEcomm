926738e667222fa579df3faa93c808b2853cdd0d
Nihar Hegde <niharhegde163@gmail.com>
2023-05-03 22:48:40 +0530

    displaying the categories on front end

--- pages/categories.js
+++ pages/categories.js
@@ -1,9 +1,15 @@
 import Layout from "@/components/Layout";
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Categories() {
   const [name, setName] = useState("");
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    axios.get("/api/categories").then((result) => {
+      setCategories(result.data);
+    });
+  }, []);
   async function saveCategory(ev) {
     ev.preventDefault();
     await axios.post("/api/categories", { name });
@@ -25,6 +31,21 @@ export default function Categories() {
           Save
         </button>
       </form>
+      <table className="basic mt-4">
+        <thead>
+          <tr>
+            <td>Category Name</td>
+          </tr>
+        </thead>
+        <tbody>
+          {categories.length > 0 &&
+            categories.map((category) => (
+              <tr key={category._id}>
+                <td>{category.name}</td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
     </Layout>
   );
 }
