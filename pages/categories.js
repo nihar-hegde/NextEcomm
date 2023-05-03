9aeec453cf39fb10a738d9f5f03401b5fee494c8
Nihar Hegde <niharhegde163@gmail.com>
2023-05-03 22:02:32 +0530

    category backend part 1

--- pages/categories.js
+++ pages/categories.js
@@ -1,12 +1,26 @@
 import Layout from "@/components/Layout";
+import axios from "axios";
+import { useState } from "react";
 
 export default function Categories() {
+  const [name, setName] = useState("");
+  async function saveCategory(ev) {
+    ev.preventDefault();
+    await axios.post("/api/categories", { name });
+    setName("");
+  }
   return (
     <Layout>
       <h1>Categories</h1>
       <label htmlFor="">New Category name</label>
       <form onSubmit={saveCategory} className="flex gap-1">
-        <input className="mb-0" type="text" placeholder="Category Name" />
+        <input
+          className="mb-0"
+          type="text"
+          placeholder="Category Name"
+          onChange={(ev) => setName(ev.target.value)}
+          value={name}
+        />
         <button type="submit" className="btn btn-primary">
           Save
         </button>
