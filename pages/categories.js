530870cc0bc72f322f7eb740eb52da35e4478462
Nihar Hegde <niharhegde163@gmail.com>
2023-05-04 22:22:11 +0530

    adding parent clusters

--- pages/categories.js
+++ pages/categories.js
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 
 export default function Categories() {
   const [name, setName] = useState("");
+  const [parentCategory, setParentCategory] = useState("");
   const [categories, setCategories] = useState([]);
   useEffect(() => {
     fetchCategories();
@@ -15,7 +16,7 @@ export default function Categories() {
   }
   async function saveCategory(ev) {
     ev.preventDefault();
-    await axios.post("/api/categories", { name });
+    await axios.post("/api/categories", { name, parentCategory });
     setName("");
     fetchCategories();
   }
@@ -31,6 +32,17 @@ export default function Categories() {
           onChange={(ev) => setName(ev.target.value)}
           value={name}
         />
+        <select
+          className="mb-0"
+          onChange={(ev) => setParentCategory(ev.target.value)}
+          value={parentCategory}
+        >
+          <option value="0">No Parent Category</option>
+          {categories.length > 0 &&
+            categories.map((category) => (
+              <option value={category._id}>{category.name} </option>
+            ))}
+        </select>
         <button type="submit" className="btn btn-primary">
           Save
         </button>
