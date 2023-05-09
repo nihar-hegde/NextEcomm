b4895648d70e3fb7dcaf17fde246967bafe07907
Nihar Hegde <niharhegde163@gmail.com>
2023-05-09 21:27:27 +0530

    displaying the assign category in edit product form

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -12,10 +12,11 @@ export default function ProductForm({
   description: existingDescription,
   price: existingPrice,
   images: existingImages,
+  category: assignedCategory,
 }) {
   const [title, setTitle] = useState(existingTitle || "");
   const [description, setDescription] = useState(existingDescription || "");
-  const [category, setCategory] = useState("");
+  const [category, setCategory] = useState(assignedCategory || "");
   const [price, setPrice] = useState(existingPrice || "");
   const [images, setImages] = useState(existingImages || []);
   const [goToProducts, setGoToProducts] = useState(false);
